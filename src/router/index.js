e4f588b49c0a2455647844c38ab86766893b71db
JhonatanC4STRO <shonano247@gmail.com>
2026-05-12 15:54:40 -0500

    Añadir layouts y rutas dinámicas del dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home      from '../views/Home.vue'
+import Home from '../views/Home.vue'
 import LoginView from '../views/LoginView.vue'
+import DashboardLayout from '../layouts/DashboardLayout.vue'
 
 const routes = [
-  { path: '/',      component: Home },
+  { path: '/', component: Home },
   { path: '/login', component: LoginView },
+  
+  // Ruta Base del Dashboard Dinámico
+  {
+    path: '/dashboard',
+    component: DashboardLayout,
+    redirect: '/dashboard/admin/resumen', // 👈 REDIRECCIÓN AÑADIDA para que no quede en blanco
+    children: [
+      // --- Vistas de Admin ---
+      { path: 'admin/resumen', component: () => import('../views/admin/AdminResumen.vue') },
+      { path: 'admin/usuarios', component: () => import('../views/admin/AdminUsuarios.vue') },
+      
+      // --- Vistas de Instructor ---
+      { path: 'instructor/resumen', component: () => import('../views/instructor/InstructorResumen.vue') },
+      
+      // --- Vistas de Aprendiz ---
+      { path: 'aprendiz/resumen', component: () => import('../views/learner/LearnerResumen.vue') }
+    ]
+  }
 ]
 
 export const router = createRouter({
