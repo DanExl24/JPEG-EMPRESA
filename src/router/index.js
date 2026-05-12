33b05a08c4785ce6342d45d56f9494e799fb2157
V44nxx <murciacorredoremerson@gmail.com>
2026-05-12 16:59:33 -0500

    recuperar contrasena

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home      from '../views/Home.vue'
-import LoginView from '../views/LoginView.vue'
+import LoginView from '../views/Login/LoginView.vue'
+import RecoverPassword from '../views/Login/RecoverPassword.vue'
 
 const routes = [
   { path: '/',      component: Home },
   { path: '/login', component: LoginView },
+  { path: '/recover', component: RecoverPassword }
 ]
 
 export const router = createRouter({
